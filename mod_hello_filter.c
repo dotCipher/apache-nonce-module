f2d359df5a5bacfdfaeefb7006e6f0607932cb22
Jill <jillian.munson@stonybrook.edu>
2013-05-03 19:32:00 -0400

    the beginning...

--- mod_hello_filter.c
+++ mod_hello_filter.c
@@ -19,3 +19,28 @@ int isEnabled;
 } HelloConfig;
 
 static void *HelloFilterConfig(apr_pool_t *p, server_rec *s
+{
+	HelloConfig *hConfig=apr_pcalloc(p, sizeof *hConfig);
+	hConfig->isEnabled=0;
+	return hConfig;
+}
+
+static void HelloFilterInsertFilter(request_rec *r)
+{
+	HelloConfig *hConfig=ap_get_module_config(r->server->module_config, &hello_filter_module);
+
+	if(!hConfig->isEnabled)
+		return;
+
+	ap_add_output_filter(s_szHelloFilterName, NULL, r,r->connection);
+}
+
+static apr_status_t HelloFilterOutFilter(ap_filter_t *f, apr_bucket_brigade *pbbIn)
+{
+	request_rec *r = f->r;
+	conn_rec *c = r->connection;
+	apr_bucket *hbktIn;
+	apr_bucket_brigade *pbbOut;
+
+	pbbOut=apr_brigade_create(r->)
+}
