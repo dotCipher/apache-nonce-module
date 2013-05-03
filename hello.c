5e4c3c5fde844d2aad4ec4dd7ac4696175539810
Ubuntu <ubuntu@ip-10-254-43-94.us-west-2.compute.internal>
2013-05-03 22:45:17 +0000

    Added helloworld module to repo

---
+++ hello.c
@@ -0,0 +1,44 @@
+#include <httpd.h>
+#include <http_protocol.h>
+#include <http_config.h>
+#include <http_core.h>
+#include <http_log.h>
+#include <http_main.h>
+#include <http_request.h>
+#include <http_connection.h>
+ 
+static int helloworld_handler(request_rec* r){
+	if (!r->handler || strcmp(r->handler, "helloworld")){
+		return DECLINED;
+	}
+    
+    ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_NOTICE, 0, r->server, "mod_hello: %s", "Before content is output");
+     
+    if (r->method_number != M_GET)
+        return HTTP_METHOD_NOT_ALLOWED;
+     
+    ap_set_content_type(r, "text/html;charset=ascii");
+    
+    if(r->header_only){
+    	return OK;
+    }
+    
+    ap_rputs("<html><head><title>Hello World!</title></head>", r);
+    ap_rputs("<body><h1>Hello World!</h1></body></html>", r);
+    return OK;
+}
+ 
+static void register_hooks(apr_pool_t* pool)
+{
+    ap_hook_handler(helloworld_handler, NULL, NULL, APR_HOOK_MIDDLE);
+}
+ 
+module AP_MODULE_DECLARE_DATA helloworld_module = {
+    STANDARD20_MODULE_STUFF,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    register_hooks
+};
