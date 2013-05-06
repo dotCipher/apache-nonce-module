ed3aa67b2aabe4127a954898a0fe65f10897d7b5
dotcipher <dotcipher@null-sec.net>
2013-05-06 14:11:55 -0400

    Minor remove

--- mod_hello_filter/modules.mk
+++
@@ -1,4 +0,0 @@
-mod_mod_hello_filter.la: mod_mod_hello_filter.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_mod_hello_filter.lo
-DISTCLEAN_TARGETS = modules.mk
-shared =  mod_mod_hello_filter.la
